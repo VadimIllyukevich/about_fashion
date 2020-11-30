2fadb8bf697f718e0947e3d02e8b22b00050cfb6
Vadzim <mr.vadikru@mail.ru>
2020-11-30 19:03:35 +0300

    real save nn

--- new/main.py
+++ new/main.py
@@ -1,13 +1,23 @@
-from train import *
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras import utils
+
+CLASSES = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+x_train = x_train.reshape(60000, 784)
+x_train = x_train / 255
+y_train = utils.to_categorical(y_train, 10)
+model = load_model('smart_nn.h5')
 
 
 def use_neural_network(self):
+    predictions = model.predict(x_train)
     plt.imshow(x_train[self].reshape(28, 28), cmap=plt.cm.binary)
     plt.show()
-    print(classes[np.argmax(predictions[self])])
-    print(classes[np.argmax(y_train[self])])
+    print(CLASSES[np.argmax(predictions[self])])
+    print(CLASSES[np.argmax(y_train[self])])
 
 
 if __name__ == "__main__":
--- new/train.py
+++ new/train.py
@@ -6,10 +6,10 @@ from tensorflow.keras import utils
 
 def train():
     model.fit(x_train, y_train, batch_size=200, epochs=10, verbose=1)
+    model.save('smart_nn.h5')
 
 
-classes = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+(x_train, y_train) = fashion_mnist.load_data()
 x_train = x_train.reshape(60000, 784)
 x_train = x_train / 255
 y_train = utils.to_categorical(y_train, 10)
@@ -17,6 +17,6 @@ model = Sequential()
 model.add(Dense(800, input_dim=784, activation="relu"))
 model.add(Dense(10, activation="softmax"))
 model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
-print(model.summary())
 train()
-predictions = model.predict(x_train)
+
+
