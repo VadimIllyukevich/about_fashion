62646bfbba91f94bfa1164f408e399ca1f54fd2d
Vadzim <mr.vadikru@mail.ru>
2020-12-02 11:57:51 +0300

    use with my image

--- new/main.py
+++ new/main.py
@@ -1,26 +1,25 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from IPython.display import Image
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
-from tensorflow.keras.datasets import fashion_mnist
-from tensorflow.keras import utils
 
 CLASSES = ['футболка', 'брюки', 'свитер', 'платье', 'пальто', 'туфли', 'рубашка', 'кроссовки', 'сумка', 'ботинки']
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-x_train = x_train.reshape(60000, 784)
-x_train = x_train / 255
-y_train = utils.to_categorical(y_train, 10)
-model = load_model('smart_nn.h5')
+IMAGEPATH = 'pictures/test_image.jpg'
 
 
-def use_neural_network(self):
-    predictions = model.predict(x_train)
-    plt.imshow(x_train[self].reshape(28, 28), cmap=plt.cm.binary)
-    plt.show()
-    print(CLASSES[np.argmax(predictions[self])])
-    print(CLASSES[np.argmax(y_train[self])])
+def use_neural_network():
+    Image(IMAGEPATH, width=150, height=150)
+    model = load_model('smart_nn.h5')
+    img = image.load_img(IMAGEPATH, target_size=(28, 28), color_mode="grayscale")
+    x = image.img_to_array(img)
+    x = x.reshape(1, 784)
+    x = 255 - x
+    x /= 255
+    prediction = model.predict(x)
+    prediction = np.argmax(prediction)
+    print("Номер класса:", prediction)
+    print("Название Класса", CLASSES[prediction])
 
 
 if __name__ == "__main__":
-    for x in range(5):
-        dressnumber = int(input())
-        use_neural_network(dressnumber)
+    use_neural_network()
